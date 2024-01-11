cd58ae0301b62be52bb11138a0e388b8dbaca254
Alex Samih-Zade <galutziogen@gmail.com>
2024-01-11 23:57:21 +0200

    arduino eth

--- src/6-routes/data-routes.ts
+++ src/6-routes/data-routes.ts
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.get("/refresh", async (request: Request, response: Response, next: NextFunction) => {
     try {
+        console.log("GOT BLYA");
         response.sendStatus(200);
     }
     catch(err: any) {
